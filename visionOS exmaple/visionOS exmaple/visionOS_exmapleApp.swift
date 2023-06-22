1c91f44d7e07c22bdf57a81e871040a22f04b65d
gaeng2y <gaeng2y@gmail.com>
2023-06-23 00:07:24 +0900

    2023 06 23 visionOS exmaple

---
+++ visionOS exmaple/visionOS exmaple/visionOS_exmapleApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionOS_exmapleApp.swift
+//  visionOS exmaple
+//
+//  Created by Kyeongmo Yang on 2023/06/23.
+//
+
+import SwiftUI
+
+@main
+struct visionOS_exmapleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
